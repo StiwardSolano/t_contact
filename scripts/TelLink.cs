ff437faa73e8749ea93f491ac896c6aa46ada715
StiwardSolano <spanker3@gmail.com>
2020-01-27 20:21:22 -0500

    Assests UP
    Scripts included

---
+++ scripts/TelLink.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TelLink : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        Application.OpenURL("tel:593984789524");
+    }
+}
---
+++ scripts/gitlink.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gitlink : MonoBehaviour
+{
+
+    private void OnMouseDown()
+    {
+        Application.OpenURL("http://github.com/stiwardsolano/");
+    }
+}
